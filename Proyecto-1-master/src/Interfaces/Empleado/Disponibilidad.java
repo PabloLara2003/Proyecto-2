a454951afff3157f35415eb7dfcd0e049d032d14
nat1305 <=>
2023-11-22 20:31:49 -0500

    empleado

--- Proyecto-1-master/src/Interfaces/Empleado/Disponibilidad.java
+++ Proyecto-1-master/src/Interfaces/Empleado/Disponibilidad.java
@@ -2,52 +2,66 @@ package Interfaces.Empleado;
 
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Disponibilidad extends JFrame {
 
-	private JPanel contentPane;
+    private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Disponibilidad frame = new Disponibilidad();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Disponibilidad frame = new Disponibilidad();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public Disponibilidad() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 286, 242);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(0, 0, 255));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    /**
+     * Create the frame.
+     */
+    public Disponibilidad() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 286, 355);
+        contentPane = new JPanel();
+        contentPane.setBackground(new Color(0, 0, 255));
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JButton btnNewButton = new JButton("diponibilidad carro ");
-		btnNewButton.setBounds(63, 53, 151, 23);
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Añadir Conductor ");
-		btnNewButton_1.setBounds(63, 113, 151, 23);
-		contentPane.add(btnNewButton_1);
-	}
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
 
+        JButton disponibilidad_C = new JButton("disponibilidad carro ");
+        disponibilidad_C.setBounds(63, 53, 151, 23);
+        contentPane.add(disponibilidad_C);
+
+        JButton añadir_C = new JButton("Añadir Conductor ");
+        añadir_C.setBounds(63, 113, 151, 23);
+        contentPane.add(añadir_C);
+
+        JButton cerrar_S = new JButton("cerrar Sesión");
+        cerrar_S.setBounds(80, 262, 114, 23);
+        contentPane.add(cerrar_S);
+
+        // Add ActionListener to the "Cerrar_S" button
+        cerrar_S.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Close the application
+                System.exit(0);
+            }
+        });
+    }
 }
+
--- Proyecto-1-master/src/Interfaces/Empleado/Usuario.java
+++ Proyecto-1-master/src/Interfaces/Empleado/Usuario.java
@@ -1,71 +1,135 @@
 package Interfaces.Empleado;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
-import java.awt.Color;
+import javax.swing.border.EmptyBorder;
 
 public class Usuario extends JFrame {
 
-	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+    private JPanel contentPane;
+    private JTextField textField;
+    private JTextField textField_1;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Usuario frame = new Usuario();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Usuario frame = new Usuario();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public Usuario() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 329, 248);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(0, 0, 255));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    /**
+     * Create the frame.
+     */
+    public Usuario() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 283, 355);
+        contentPane = new JPanel();
+        contentPane.setBackground(new Color(0, 0, 255));
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		textField = new JTextField();
-		textField.setBounds(126, 46, 96, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(126, 116, 96, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JTextPane txtpnUsuario = new JTextPane();
-		txtpnUsuario.setForeground(new Color(255, 255, 255));
-		txtpnUsuario.setBackground(new Color(0, 0, 255));
-		txtpnUsuario.setText("Usuario");
-		txtpnUsuario.setBounds(53, 46, 49, 20);
-		contentPane.add(txtpnUsuario);
-		
-		JTextPane txtpnContrasea = new JTextPane();
-		txtpnContrasea.setForeground(new Color(255, 255, 255));
-		txtpnContrasea.setBackground(new Color(0, 0, 255));
-		txtpnContrasea.setText("Contraseña");
-		txtpnContrasea.setBounds(53, 116, 63, 20);
-		contentPane.add(txtpnContrasea);
-	}
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(125, 84, 96, 20);
+        contentPane.add(textField);
+        textField.setColumns(10);
+
+        textField_1 = new JTextField();
+        textField_1.setBounds(125, 154, 96, 20);
+        contentPane.add(textField_1);
+        textField_1.setColumns(10);
+
+        JTextPane Usuario = new JTextPane();
+        Usuario.setText("Usuario:");
+        Usuario.setForeground(Color.WHITE);
+        Usuario.setBackground(Color.BLUE);
+        Usuario.setBounds(53, 84, 49, 20);
+        contentPane.add(Usuario);
+
+        JTextPane Contrasena = new JTextPane();
+        Contrasena.setText("Contraseña:");
+        Contrasena.setForeground(Color.WHITE);
+        Contrasena.setBackground(Color.BLUE);
+        Contrasena.setBounds(52, 154, 63, 20);
+        contentPane.add(Contrasena);
+
+        JTextPane IniciarSesion = new JTextPane();
+        IniciarSesion.setText("Iniciar Sesión");
+        IniciarSesion.setForeground(Color.WHITE);
+        IniciarSesion.setBackground(Color.BLUE);
+        IniciarSesion.setBounds(106, 22, 78, 20);
+        contentPane.add(IniciarSesion);
+
+        JButton btnNewButton = new JButton("Continuar");
+        btnNewButton.setBounds(95, 228, 89, 23);
+        contentPane.add(btnNewButton);
+        
+        JButton CrearUsuario = new JButton("Crear Usuario");
+        CrearUsuario.setBounds(80, 262, 115, 23);
+        contentPane.add(CrearUsuario);
+
+        // Add ActionListener to the "CrearUsuario" button
+        CrearUsuario.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Move to the crear_Usuario frame
+                crear_Usuario crearUsuarioFrame = new crear_Usuario();
+                crearUsuarioFrame.setVisible(true);
+                dispose(); // Close the current frame
+            }
+        });
+
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (validarCredenciales()) {
+                    // Move to the Disponibilidad frame
+                    Disponibilidad opcionesFrame = new Disponibilidad();
+                    opcionesFrame.setVisible(true);
+                    dispose(); // Close the current frame
+                } else {
+                    System.out.println("Credenciales incorrectas");
+                }
+            }
+        });
+    }
+
+    private boolean validarCredenciales() {
+        String usuario = textField.getText();
+        String contraseña = textField_1.getText();
+        String filePath = ".data/Crear_Usuario.csv";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 2 && parts[0].equals(usuario) && parts[1].equals(contraseña)) {
+                    return true; // Credentials found
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false; // Credentials not found
+    }
 }
